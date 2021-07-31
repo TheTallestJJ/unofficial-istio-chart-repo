79060a71b8bd7513052ce37e4c9b6dad97124a31
Jeroen Jetten <me@jeroen-jetten.com>
2021-07-31 13:11:02 +0000

    refine github action

--- .github/scripts/retrieve-charts.sh
+++ .github/scripts/retrieve-charts.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash -xe
 
 curl https://api.github.com/repos/istio/istio/releases | jq '.[].tag_name' -r > versions-new
 diff .versions versions-new | grep '>' | sed 's/> //g' > diff-versions
@@ -6,12 +6,13 @@ diff .versions versions-new | grep '>' | sed 's/> //g' > diff-versions
 for version in $(cat diff-versions); do
   git clone --depth 1 --branch "$version" https://github.com/istio/istio "$version"
   for chart in $(cat .charts); do
-    helm package "$version"/$chart -version $version
+    helm package "$version"/$chart --version $version --destination charts
   done
+  rm -rf "$version"
 done
 
 rm diff-versions
 mv versions-new .versions
 
-helm repo index
+helm repo index .
 
